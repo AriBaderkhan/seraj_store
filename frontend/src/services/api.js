f3507b714a40c9b1235b0a5b540a745643ef9dee
AriBaderkhan <ari99baderkhan@gmail.com>
2026-01-27 10:32:16 +0300

    frontend change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:1999', // Backend URL
+    baseURL: import.meta.env.VITE_API_BASE_URL, // Backend URL
     headers: {
         'Content-Type': 'application/json',
     },
